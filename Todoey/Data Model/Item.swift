6f614c4bde9eb6b3916c4d96209b15a9cf167f4e
Hani Joudeh <hanijoudeh@icloud.com>
2019-11-23 13:13:17 -0600

    convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Hani Joudeh on 11/23/19.
+//  Copyright © 2019 Hani Joudeh. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
